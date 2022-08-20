ccd8b14a3ea8add9d30a27430dde4c502fb42a36
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-21 00:33:41 +0300

    installed react icons library

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {BsStack} from 'react-icons/bs';
+import {FaHome} from 'react-icons/fa';
+import {BiCoinStack} from 'react-icons/bi';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       <ul className='sidebar-items'>
-        <li>sidebar item</li>
-        <li>sidebar item</li>
-        <li>sidebar item</li>
+        <li className='icon'><FaHome  /></li>
+        <li className='icon'><BsStack /></li>
+        <li className='icon'><BiCoinStack /></li>
       </ul>
     </div>
   )
