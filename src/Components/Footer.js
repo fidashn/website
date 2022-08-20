a5316a3662b8833fbdd4cba8f2d2a01beb01bc47
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-20 17:23:34 +0300

    added navbar & footer components

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>כל הזכויות שמורות&copy;</div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <ul className='navbar-items'>
+        <li>דף הבית</li>
+        <li>פיננסי</li>
+        <li>טכנולוגי</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
