c772034bcbf3b40a20e3e685b3f56a350f42bf64
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-21 00:37:05 +0300

    added main file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './Components/Footer';
 import Sidebar from './Components/Sidebar';
+import Main from './Components/Main';
 
 
 function App() {
   return (
     <div className="App">
+      <Main />
      <Sidebar />
      <Footer />
     </div>
