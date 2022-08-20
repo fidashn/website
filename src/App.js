33c9da0b7471f0dae577bcb09a778b7bd53b8ad4
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-20 17:31:56 +0300

    deleted navbar & created sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Footer from './Components/Footer';
-import Navbar from './Components/Navbar';
+import Sidebar from './Components/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
+     <Sidebar/>
      <Footer />
     </div>
   );
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <ul className='sidebar-items'>
+        <li>sidebar item</li>
+        <li>sidebar item</li>
+        <li>sidebar item</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
