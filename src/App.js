09f2b52cc460f6e89897a4b03e895c99d0bfe523
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-20 17:42:43 +0300

    editted display settings

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Sidebar from './Components/Sidebar';
 function App() {
   return (
     <div className="App">
+      <header></header>
+      <main></main>
      <Sidebar/>
      <Footer />
     </div>
