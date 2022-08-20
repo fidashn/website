36b4acbc2cab02de642e7e6fe4582114f2569245
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-21 00:59:03 +0300

    added borders to all items each with its' color

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import Main from './Components/Main';
 function App() {
   return (
     <div className="App">
+     <div className='page-container'>
      <Sidebar />
       <Main />
+      </div>
      <Footer />
     </div>
   );
