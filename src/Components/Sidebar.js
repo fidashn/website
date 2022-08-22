12902161fd5d33eca77a1eebf0e917b426e2ab05
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-22 15:51:36 +0300

    used fontawesome icons successfully

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,19 +1,14 @@
 import React from 'react';
-// import {BsStack} from 'react-icons/bs';
-// import {FaHome} from 'react-icons/fa';
-// import {BiCoinStack} from 'react-icons/bi';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHouse, faCoins, faLayerGroup} from '@fortawesome/free-solid-svg-icons';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       <ul className='sidebar-items'>
-        {/* <li className='icon'><div className='home-icon'><FaHome/></div></li>
-        <li className='icon'><BsStack /></li>
-        <li className='icon'><BiCoinStack /></li> */}
-        <li><FontAwesomeIcon icon="fa-house"  />דף הבית</li>
-        <li><FontAwesomeIcon icon="fa-coins"  />פיננסי</li>
-        <li><FontAwesomeIcon icon="fa-layer-group"  />טכנולוגי</li>
+        <li><FontAwesomeIcon icon={faHouse} />דף הבית</li>
+        <li><FontAwesomeIcon icon={faCoins}  />פיננסי</li>
+        <li><FontAwesomeIcon icon={faLayerGroup}  />טכנולוגי</li>
       </ul>
     </div>
   )
