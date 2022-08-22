acee88194b8a2f2571deeeb12dddd3fa0d073f8f
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-22 15:42:18 +0300

    installed fontawesome library

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {BsStack} from 'react-icons/bs';
-import {FaHome} from 'react-icons/fa';
-import {BiCoinStack} from 'react-icons/bi';
+// import {BsStack} from 'react-icons/bs';
+// import {FaHome} from 'react-icons/fa';
+// import {BiCoinStack} from 'react-icons/bi';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       <ul className='sidebar-items'>
-        <li className='icon'><div className='home-icon'><FaHome/></div></li>
+        {/* <li className='icon'><div className='home-icon'><FaHome/></div></li>
         <li className='icon'><BsStack /></li>
-        <li className='icon'><BiCoinStack /></li>
+        <li className='icon'><BiCoinStack /></li> */}
+        <li><FontAwesomeIcon icon="fa-house"  />דף הבית</li>
+        <li><FontAwesomeIcon icon="fa-coins"  />פיננסי</li>
+        <li><FontAwesomeIcon icon="fa-layer-group"  />טכנולוגי</li>
       </ul>
     </div>
   )
