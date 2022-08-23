b25e889b5e21bd3c3ddc8a645cc5a6e503e7cce8
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-23 10:58:03 +0300

    moved the text properly under the icons

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -6,9 +6,9 @@ function Sidebar() {
   return (
     <div className='sidebar'>
       <ul className='sidebar-items'>
-        <li><div><FontAwesomeIcon icon={faHouse} size="3x" className='icons'/><p>דף הבית</p> </div> </li>
-        <li><div><FontAwesomeIcon icon={faCoins} size="3x" className='icons'/><p>פיננסי</p></div></li>
-        <li><div><FontAwesomeIcon icon={faLayerGroup} size="3x" className='icons'/><p>טכנולוגי</p></div></li>
+        <li><div  className='icons'><FontAwesomeIcon icon={faHouse} size="3x"/><p>דף הבית</p> </div> </li>
+        <li><div  className='icons'><FontAwesomeIcon icon={faCoins} size="3x" /><p>פיננסי</p></div></li>
+        <li><div  className='icons'><FontAwesomeIcon icon={faLayerGroup} size="3x" /><p>טכנולוגי</p></div></li>
       </ul>
     </div>
   )
