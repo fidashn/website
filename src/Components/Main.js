1a7ab2a3e203b563ede9b08c1b30c7abe2173f4e
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-24 20:19:38 +0300

    added picture to ads section

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,7 +4,7 @@ function main() {
   return (
     <div className='main-content'>
       <p className='main-content-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ea delectus cupiditate fugit autem mollitia beatae aliquam provident. Iusto adipisci totam nisi illum. Iusto dolor fugit mollitia delectus, similique expedita eveniet earum minus necessitatibus? Odio, unde ea id corporis ullam, soluta ad cumque voluptate possimus molestias obcaecati? Tempora dolorem quam quos sit, voluptatum illum aut iusto ea est, dolore dolor vitae quidem delectus beatae ipsum fugit earum hic ad? Fuga, ipsam est dolorem temporibus rerum itaque ipsa vero ducimus corporis facere architecto ratione pariatur aspernatur? Quo suscipit, exercitationem magnam, consectetur vel tenetur itaque tempore soluta hic perferendis ab nam deserunt minus beatae deleniti. Cumque est, quo, rem similique ut id perferendis ipsum consequatur, eligendi impedit quos ex totam veniam repudiandae amet. Vero pariatur magnam fugit cum veritatis debitis ut odit repellendus. Ducimus corporis iure illo perspiciatis ad atque doloribus adipisci perferendis, debitis accusamus quae, enim recusandae voluptatibus optio quisquam velit non nobis sed modi nihil? Cumque magnam tempora omnis debitis animi facere praesentium officia quod quo ratione ea vero sed similique repellendus modi exercitationem repudiandae, nesciunt ipsam. Dolorum, pariatur. Vel iusto debitis nisi, tempore eum architecto? Eum laudantium repellat perspiciatis quia. Provident, dolorum adipisci. Laboriosam ullam repudiandae ducimus porro obcaecati!</p>
-      <p className='ads'>ads</p>
+      <p className='ads'></p>
       </div>
   )
 }
