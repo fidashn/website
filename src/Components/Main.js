92e197e7a0053fb4048fb08756017c68bafef439
fidashn <87206839+fidashn@users.noreply.github.com>
2022-08-25 19:32:37 +0300

    added a title to main

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,7 +3,9 @@ import React from 'react'
 function main() {
   return (
     <div className='main-content'>
-      <p className='main-content-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam doloribus possimus reprehenderit fuga necessitatibus ex quia tenetur sed, aliquid est! Id modi animi vitae obcaecati neque nemo nihil nesciunt perferendis consequatur nulla, quod officiis, architecto delectus magni totam illum aperiam reprehenderit distinctio error repudiandae? Rerum dolores cumque labore soluta! Inventore tempore quibusdam, nam a error est tenetur saepe delectus cum repellat ipsum assumenda eius, officiis officia, nisi eveniet corrupti aliquam.</p>
+      <p className='main-content-p'>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam doloribus possimus reprehenderit fuga necessitatibus ex quia tenetur sed, aliquid est! Id modi animi vitae obcaecati neque nemo nihil nesciunt perferendis consequatur nulla, quod officiis, architecto delectus magni totam illum aperiam reprehenderit distinctio error repudiandae? Rerum dolores cumque labore soluta! Inventore tempore quibusdam, nam a error est tenetur saepe delectus cum repellat ipsum assumenda eius, officiis officia, nisi eveniet corrupti aliquam.</p>
       <p className='ads'></p>
       </div>
   )
